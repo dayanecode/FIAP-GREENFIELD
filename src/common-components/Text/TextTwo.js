7a5ba29a0500643010f0820b65fffa71208023ae
Dayane Ramos <dayanecode@gmail.com>
2022-12-02 08:54:45 -0300

    color changed to orange_greenfield

--- src/common-components/Text/TextTwo.js
+++ src/common-components/Text/TextTwo.js
@@ -5,7 +5,8 @@ export const TextTwo = styled.span`
     font-family: ${FontFamilies.PRIMARY};
     font-weight: ${FontWeights.ULTRA_BOLD};
     font-size: ${FontSizes.TWO};
-    color: ${Colors.NEUTRAL_BLACK};
+    color: ${Colors.LARANJA_GREENFIELD};
+    text-shadow: 1px 1px ${Colors.BEGE_CLARO_GREENFIELD}; ;
     margin-top: ${Spaces.ONE};
     margin-right: ${Spaces.TEN};
 `;
